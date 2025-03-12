d5f28b2988dc012441f63defd888759c52f2d79c
Gabriel Fernandes <saygabrielfernandes3105@gmail.com>
2025-03-12 10:34:27 -0300

    Aula sobre variaveis e decisoes

---
+++ aula02/decisao.php
@@ -0,0 +1,22 @@
+<?php
+    $n = 6;
+
+    if ($n >= 4) {
+            echo $n, " é maior ou igual a 4 <br> ";
+            echo "<br>";
+    }
+    if ($n) {
+        echo "a variavel tem valor !";
+    }
+
+    $n1 = 3;
+    $n2 = 4;
+
+    $result = ($n1/$n2)%2;
+
+    if ($result == 1) {
+        echo "<br>" , $result, "quociente impar de novo"; 
+    } else {
+        echo "<br> ", $result, "quociente de par.";
+    }
+    ?>
---
+++ aula02/variaveis.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+$texto = "Isso é um texto.";
+$n1 = 3;
+$n2 = 3.14;
+$valor = null;
+
+echo $texto;
+echo "<br>";
+echo gettype($texto), "<br>";
+echo gettype($n1), "<br>";
+echo gettype($n2), "<br>";
+echo gettype($valor), "<br>";
+
+
+
+?>
+
+
+</body>
+</html>
