a7dc6936231c823e15dae79382f2bfd047c77488
Gabriel Fernandes <saygabrielfernandes3105@gmail.com>
2025-03-19 10:31:18 -0300

    Aula sobre estruturas de repetição em PHP

---
+++ aula04/repeticao.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+
+$i = 1;
+
+while ($i < 6 ) {
+    echo $i;
+    echo "<br>";
+    $i ++;
+}
+
+echo "-------------<br>";
+$i = 0;
+    do {
+        echo $i;
+        $i ++;
+        echo "<br>";
+    } while( $i < 6);
+
+echo    "-----------<br>";
+
+for ($x = 0; $x <= 10; $x++); {
+    echo "o número é: $x <br>";
+}
+
+
+
+
+
+
+
+?>
+</body>
+</html>
